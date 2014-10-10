957283926375ed655ddf0c0ae5a8ec7c354955f4
Piyush <achyutapiyush@gmail.com>
2014-10-11 00:27:07 +0530

    Making another project for EDF. Will merge after finalizing

---
+++ EDF schedule/EDF/include/Process.h
@@ -0,0 +1,21 @@
+#ifndef PROCESS_H
+#define PROCESS_H
+
+
+class Process
+{
+    public:
+        int execution_time;
+        bool is_periodic;
+        int period;
+        int relative_deadline;
+        int absolute_deadline;
+
+        Process();
+        virtual ~Process();
+        bool set_period(int);
+    protected:
+    private:
+};
+
+#endif // PROCESS_H
---
+++ EDF schedule/EDF/src/Process.cpp
@@ -0,0 +1,17 @@
+#include "Process.h"
+
+Process::Process()
+{
+    //ctor
+}
+
+Process::~Process()
+{
+    //dtor
+}
+
+bool set_period(int userperiod)
+{
+    is_periodic = true;
+    period = userperiod;
+}
