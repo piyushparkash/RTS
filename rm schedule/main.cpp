107bc15168f6a8a67e082b5cf62be88959d10aae
jashnmalhi <jashnmalhi@yahoo.in>
2014-10-11 02:36:08 +0530

    correction in priority allocation and utilization bound

--- rm schedule/main.cpp
+++ rm schedule/main.cpp
@@ -1,15 +1,17 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 int main()
 {
-     int select,process,a_temp;
-     float n,uti=0;
-     //get total number of processes
+     long int select,process,a_temp;
+//get total number of processes
      cout <<"How many total processes";
      cin>>process;
 
-     int p_time[process],ex_time[process],prior[process],prior_temp[process];
-    //get the C and T of all processes
+     float p_time[process],ex_time[process],n,uti=0,process_f;
+     process_f=process;
+     int prior[process],prior_temp[process];
+//get the C and T of all processes
      for(int i=0;i<process;i++)
      {
         cout<<"\ncompletion Time or execution time of process"<<i+1<<": ";
@@ -17,25 +19,28 @@ int main()
         cout<<"\nperiod of process"<<i+1<<": ";
         cin>>p_time[i];
      }
-     //copy the period of all processes in another variable to get priority
+//copy the period of all processes in another variable to get priority
      for(int k=0;k<process;k++)
         {
             prior_temp[k]=p_time[k];
         }
-    //sort the period to set priority
-     for(int k=0;k<process;k++)
+//sort the period to set priority
+     for(int k=0;k<process-1;k++)
         {
-            for(int j=0;j<process;j++)
+            for(int j=0;j<process-1;j++)
                 {
                     if(prior_temp[j]>prior_temp[j+1])
                         {
                                 a_temp=prior_temp[j];
+                                //cout<<a_temp<<"  "<<j<<"--a\t";
                                 prior_temp[j]=prior_temp[j+1];
+                                //cout<<prior_temp[j]<<"--prio j\t";
                                 prior_temp[j+1]=a_temp;
+                                //cout<<prior_temp[j+1]<<"--prio j+1\t";
                         }
                 }
          }
-    //match the priority to original processes
+//match the priority to original processes
     for(int k=0;k<process;k++)
         {
             for(int j=0;j<process;j++)
@@ -46,50 +51,58 @@ int main()
                           }
                  }
         }
-    //print the table
+//print the table
      cout<<"\nprocess \t ex-time \t period \t priority \t response time \n";
      for(int j=0;j<process;j++)
      {
         cout<<"p"<<j<<" \t\t "<<ex_time[j]<<" \t\t "<<p_time[j]<<" \t\t "<<prior[j]<<" \t\t "<<"\n";
      }
-     // get the utilization bound for condition u<=n*(2^(1/n) -1)
     cout<<"\n";
+// get the utilization bound for condition u<=n*(2^(1/n) -1)
     for(int a=0;a<process;a++)
     {
-        uti=uti+(ex_time[a]/p_time[a]);
+       uti=uti+(ex_time[a]/p_time[a]);
     }
-
-    n=process*((2^(1/process))-1);
+    process_f=process;
+    cout<<process_f;
+    n=process*(pow(2.0,1/process_f)-1);
     cout<<uti <<"\t"<< n<<"\n";
+//if utilization condition satisfied print the algo choice
     if(uti<=n)
     {
         cout<<"the tasks are scheduleable !! utilization="<<uti<<"\n";
+//ask for the scheduling algo
+        cout <<"choose 1 algo"<<"\n1. RM scheduling"<<"\n2. EDF scheduling \n";
+        cin>>select;
+//if choice invalid
+        if(select<1 || select>2)
+        {
+             cout<<"invalid digit !! try again";
+             cout <<"choose 1 algo"<<"\n1. RM scheduling"<<"\n2. EDF scheduling \n";
+        cin>>select;
+        }
+        cout<<"\n";
+// switch to the chosen algo
+        switch(select)
+        {
+    // RM algo logic
+            case 1:  cout<<"RM scheduling algo\n";
+
+            break;
+    //EDF algo logic
+            case 2:  cout<<"EDF scheduling algo";
+            break;
+    //default choice
+            default:  cout<<"no selection";
+            break;
+        }
     }
+// if the condition is not satisfied end the execution
     else
     {
         cout<<"failed to schedule the tasks !! utilization="<<uti<<" n="<<n<<"\n";
     }
 
-/*
-    cout <<"choose 1 algo"<<"\n1. RM scheduling"<<"\n2. EDF scheduling \n";
-    cin>>select;
-    if(select<1 || select>2)
-    {
-         cout<<"invalid digit !! try again";
-         cout <<"choose 1 algo"<<"\n1. RM scheduling"<<"\n2. EDF scheduling \n";
-    cin>>select;
-    }
-cout<<"\n";
-    switch(select)
-    {
-        case 1:  cout<<"RM scheduling algo\n";
 
 
-        break;
-        case 2:  cout<<"EDF scheduling algo";
-        break;
-        default:  cout<<"no selection";
-        break;
-    }
-*/
 }
