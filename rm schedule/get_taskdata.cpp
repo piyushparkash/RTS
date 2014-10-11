3d49d25dc8ec2b79861334ed0e2fc468a556642a
jashnmalhi <jashnmalhi@yahoo.in>
2014-10-11 20:15:49 +0530

    in class

---
+++ rm schedule/get_taskdata.cpp
@@ -0,0 +1,109 @@
+#include "get_taskdata.h"
+#include<iostream>
+#include <cmath>
+#include <windows.h>
+get_taskdata::get_taskdata(int process_temp)
+{
+    process=process_temp;
+    float p_time[process],ex_time[process],n,uti=0,process_f,response[process];
+    process_f=process;
+    int prior[process],prior_temp[process];
+//get the C and T of all processes
+     for(int i=0;i<process;i++)
+     {
+        cout<<"\ncompletion Time or execution time of process"<<i+1<<": ";
+        cin>>ex_time[i];
+        cout<<"\nperiod of process"<<i+1<<": ";
+        cin>>p_time[i];
+     }
+//copy the period of all processes in another variable to get priority
+     for(int k=0;k<process;k++)
+        {
+            prior_temp[k]=p_time[k];
+        }
+//sort the period to set priority
+     for(int k=0;k<process-1;k++)
+        {
+            for(int j=0;j<process-1;j++)
+                {
+                    if(prior_temp[j]>prior_temp[j+1])
+                        {
+                                a_temp=prior_temp[j];
+                                //cout<<a_temp<<"  "<<j<<"--a\t";
+                                prior_temp[j]=prior_temp[j+1];
+                                //cout<<prior_temp[j]<<"--prio j\t";
+                                prior_temp[j+1]=a_temp;
+                                //cout<<prior_temp[j+1]<<"--prio j+1\t";
+                        }
+                }
+         }
+//match the priority to original processes
+    for(int k=0;k<process;k++)
+        {
+            for(int j=0;j<process;j++)
+                 {
+                      if(p_time[k]== prior_temp[j])
+                          {
+                              prior[k]=j+1;
+                          }
+                 }
+        }
+        //print the table
+     cout<<"\nprocess \t ex-time \t period \t priority \n";
+     for(int j=0;j<process;j++)
+     {
+        cout<<"p"<<j<<" \t\t "<<ex_time[j]<<" \t\t "<<p_time[j]<<" \t\t "<<prior[j]<<" \t\t "<<"\n";
+     }
+    cout<<"\n";
+    // get the utilization bound for condition u<=n*(2^(1/n) -1)
+    for(int a=0;a<process;a++)
+    {
+       uti=uti+(ex_time[a]/p_time[a]);
+    }
+    process_f=process;
+    n=process*(pow(2.0,1/process_f)-1);
+//if utilization condition satisfied print the algo choice
+    if(uti<=n)
+    {
+        cout<<"the tasks are scheduleable !! utilization="<<uti<<"\n";
+//ask for the scheduling algo
+        cout <<"choose 1 algo"<<"\n1. RM scheduling"<<"\n2. EDF scheduling \n";
+        cin>>select;
+//if choice invalid
+        if(select<1 || select>2)
+        {
+             cout<<"invalid digit !! try again";
+             cout <<"choose 1 algo"<<"\n1. RM scheduling"<<"\n2. EDF scheduling \n";
+        cin>>select;
+        }
+        cout<<"\n";
+// switch to the chosen algo
+        switch(select)
+        {
+    // RM algo logic
+            case 1:  cout<<"RM scheduling algo\n";
+            for(int k=0;k<process;k++)
+            {
+                cout<<"process "<<k+1<<" expected time "<<ex_time[k]<<"sec\t executing task "<<k+1<<"....\t";
+                cout.flush();
+                Sleep(ex_time[k]*1000);
+                cout<<"task "<<k+1<<"executed\n";
+            }
+            break;
+    //EDF algo logic
+            case 2:  cout<<"EDF scheduling algo";
+            break;
+    //default choice
+            default:  cout<<"no selection";
+            break;
+        }
+    }
+// if the condition is not satisfied end the execution
+    else
+    {
+        cout<<"failed to schedule the tasks !! utilization="<<uti<<" n="<<n<<"\n";
+    }
+
+}
+
+
---
+++ rm schedule/get_taskdata.h
@@ -0,0 +1,8 @@
+using namespace std;
+class get_taskdata
+{
+private:
+     long int select,process,a_temp;
+public:
+    get_taskdata(int process_temp);
+};
