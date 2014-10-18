6da90f71a560f3cd3a94c41358475fa1146c6880
Piyush <achyutapiyush@gmail.com>
2014-10-18 11:38:32 +0530

    Implementing File IO

--- EDF schedule/EDF/main.cpp
+++ EDF schedule/EDF/main.cpp
@@ -5,16 +5,17 @@ using namespace std;
 
 int main()
 {
+    Schedule sch;
+    sch.loadProcessFromFile("Sample.txt");
     int choice;
     cout<<"choose the algo to proceed \n 1.) EDF scheduling algo \n 2.) RM scheduling algo\n";
     cin>>choice;
-    if(choice<1 || choice>2)
+    if(choice < 1 || choice > 2)
     {
        cout<<"invalid digit !! try again\n";
        cout<<"choose the algo to proceed \n 1.) EDF scheduling algo \n 2.) RM scheduling algo\n";
        cin>>choice;
     }
-    Schedule sch;
     switch(choice)
     {
        case(1):
